84b4765614697598e354ebbc8dc6fd4ddf498205
wangjoshuah <josh.wang@optimizely.com>
2016-05-10 16:37:44 -0700

    create new project through XCode

---
+++ TVML-demo-app/TVML-demo-app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TVML-demo-app
+//
+//  Created by Josh Wang on 5/10/16.
+//  Copyright © 2016 wangjoshuah. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
