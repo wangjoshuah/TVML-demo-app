71fbe465c6e7ed90d76a1e0a0e0b856d34ed41ae
wangjoshuah <josh.wang@optimizely.com>
2016-05-10 20:26:08 -0700

    flesh out catalog template

--- client/templates/RWDevConTemplate.xml.js
+++ client/templates/RWDevConTemplate.xml.js
@@ -1,9 +1,18 @@
+
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <document>
     <catalogTemplate>
       <banner>
         <title>RWDevConHighlights</title>
       </banner>
+      <list>
+        <section>
+          <listItemLockup>
+            <title>Inspiration Videos</title>
+            <decorationLabel>13</decorationLabel>
+          </listItemLockup>
+        </section>
+      </list>
     </catalogTemplate>
   </document>`
 }
