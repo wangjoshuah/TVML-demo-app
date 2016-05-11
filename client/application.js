0285dd881371399c9deec3f7ffa58afa85d49a50
wangjoshuah <josh.wang@optimizely.com>
2016-05-10 17:04:20 -0700

    add application.js for client logic

---
+++ client/application.js
@@ -0,0 +1,19 @@
+App.onLaunch = function(options) {
+  // 1 use Navigation Controller to push an alert
+  var alert = createAlert("Hello World", ""); //leaving 2nd parameter with an empty string
+  navigationDocument.presentModal(alert);
+}
+
+// 2 create an Alert View
+var createAlert = function(title, description) {
+  var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
+    <document>
+      <alertTemplate>
+        <title>${title}</title>
+        <description>${description}</description>
+      </alertTemplate>
+    </document>`
+    var parser = new DOMParser();
+    var alertDoc = parser.parseFromString(alertString, "application/xml");
+    return alertDoc
+}
